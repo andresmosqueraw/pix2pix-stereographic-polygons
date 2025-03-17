50ac91277ab9f1d9e35a51fb7db14c279ec7254e
andrew mos <andresmosquera739@gmail.com>
2025-03-17 16:33:00 -0500

    Initial commit

---
+++ postprocess.py
@@ -0,0 +1,308 @@
+import glob
+import os
+import cv2
+import argparse
+import shutil
+import sys
+
+import numpy as np
+import csv
+
+
+def merge(FLAGS):
+  """
+
+  """
+
+  if not os.path.isdir(FLAGS.test_dir):
+    raise ValueError("Could not find test_dir.")
+
+  if not os.path.isdir(FLAGS.input_dir):
+    raise ValueError("Could not find input_dir.")
+
+  if not os.path.isdir(FLAGS.target_dir):
+    raise ValueError("Could not find target_dir.")
+
+  if not os.path.isdir(FLAGS.output_dir):
+    os.makedirs(FLAGS.output_dir)
+
+  test_list = sorted(glob.glob(os.path.join(FLAGS.test_dir, '*.png')))
+  input_list = sorted(glob.glob(os.path.join(FLAGS.input_dir, '*.png')))
+
+  for inp in input_list:
+    input_base = os.path.splitext(os.path.basename(inp))[0]
+    input_word = input_base + "_"
+    inp_img = cv2.imread(inp)
+
+    inp_height, inp_width = inp_img.shape[:2]
+    # inp_height, inp_width = 256*6, 256*11
+
+    test_list_ex = [img for img in test_list if input_word in img]
+    test_list_output = [img for img in test_list_ex if "-outputs" in img]
+
+    img_row_max = 0
+    img_col_max = 0
+
+    # img_name_array1d = []
+    img_array1d = []
+    for img in test_list_output:
+      img_base = os.path.splitext(os.path.basename(img))[0].replace('-outputs', '')
+
+      img_row = int(img_base.rsplit("_", 2)[-2])-1
+      img_col = int(img_base.rsplit("_", 2)[-1])-1
+
+      if img_row > img_row_max:
+        img_row_max = img_row
+      if img_col > img_col_max:
+        img_col_max = img_col
+      # print(img_row_max, img_col_max)
+
+      if (img_row == 0) & (img_col == 0):
+        img_img = cv2.imread(img)
+        # img_img = imread(img)
+        img_height, img_width = img_img.shape[:2]
+
+      # img_name_array1d.append(img)
+      # img_name_array1d.append((img_row, img_col))
+      img_array1d.append(cv2.imread(img))
+
+    # img_name_array = [img_name_array1d[i:i+img_col_max+1] for i in range(0, len(img_name_array1d), img_col_max+1)]
+    img_array = [img_array1d[i:i+img_col_max+1] for i in range(0, len(img_array1d), img_col_max+1)]
+
+    # print(img_name_array)
+    # print(len(img_name_array), len(img_name_array[0]))
+
+    width = inp_width
+    w_split = img_width
+    height = inp_height
+    h_split = img_height
+    print(height, width)
+    print(h_split, w_split)
+
+    # if width <= w_split:
+    if img_col_max == 0:
+      pad_h = [(0, width)]
+    else:
+      n_h, rem_h = divmod(width, w_split)
+      if rem_h == 0:
+        overlap_h = 0
+        err_h = 0
+      else:
+        overlap_h = (w_split - rem_h) // n_h
+        sum_h = (w_split - overlap_h) * n_h + w_split
+        err_h = sum_h - width
+        n_h += 1
+      pad_h = list(range(n_h))
+      left_next = 0
+      for i_h in range(n_h):
+        pad_L = overlap_h // 2
+        pad_R = overlap_h - pad_L
+
+        left = left_next
+        right = left + w_split
+
+        if i_h == 0:
+          left = 0
+        elif i_h < err_h +1:
+          left = pad_L + 1
+        else:
+          left = pad_L
+
+        if i_h == n_h - 1:
+          right = w_split
+        else:
+          right = w_split - pad_R
+        # # left_next = right - overlap_h
+        # elif i_h < err_h:
+        #   left_next -= 1
+        # print(i_h, left, right, left_next)
+        pad_h[i_h] = (left, right)
+
+      # print(pad_h)
+
+
+    # if height <= h_split:
+    if img_row_max == 0:
+      pad_v = [(0, height)]
+    else:
+      n_v, rem_v = divmod(height, h_split)
+      if rem_v == 0:
+        overlap_v = 0
+        err_v = 0
+      else:
+        overlap_v = (h_split - rem_v) // n_v
+        sum_v = (h_split - overlap_v) * n_v + h_split
+        err_v = sum_v - height
+        n_v += 1
+      pad_v = list(range(n_v))
+      left_next = 0
+      for i_v in range(n_v):
+        pad_U = overlap_v // 2
+        pad_B = overlap_v - pad_U
+
+        left = left_next
+        right = left + h_split
+
+        if i_v == 0:
+          left = 0
+        elif i_v < err_v +1:
+          left = pad_U + 1
+        else:
+          left = pad_U
+
+        if i_v == n_v - 1:
+          right = h_split
+        else:
+          right = h_split - pad_B
+
+        pad_v[i_v] = (left, right)
+
+      # print(pad_v)
+
+
+    img_array_pad = [[0 for x in range(img_col_max+1)] for y in range(img_row_max+1)]
+    # print(img_array_pad)
+    for col in range(img_col_max+1):
+      for row in range(img_row_max+1):
+        img_array_pad[row][col] = img_array[row][col][pad_v[row][0]:pad_v[row][1], pad_h[col][0]:pad_h[col][1]]
+
+    if img_row_max == 0:
+      # img_array_h = [img_tmp for img_tmp_1 in img_array for img_tmp in img_tmp_1]
+      img_merge = cv2.hconcat(img_array_pad[0])
+    else:
+      img_merge = cv2.vconcat([cv2.hconcat(img_array_h) for img_array_h in img_array_pad])
+
+    print(img_merge.shape[:2])
+
+    cv2.imwrite(os.path.join(FLAGS.output_dir, input_base + ".png"), img_merge)
+
+
+def analyze(FLAGS):
+  if not os.path.isdir(FLAGS.input_dir):
+    raise ValueError("Could not find input_dir.")
+
+  if not os.path.isdir(FLAGS.target_dir):
+    raise ValueError("Could not find target_dir.")
+
+  if not os.path.isdir(FLAGS.output_dir):
+    raise ValueError("Could not find output_dir.")
+
+
+  input_list = sorted(glob.glob(os.path.join(FLAGS.input_dir, '*.png')))
+
+  target_list = sorted(glob.glob(os.path.join(FLAGS.target_dir, '*.png')))
+
+  output_list = sorted(glob.glob(os.path.join(FLAGS.output_dir, '*.png')))
+
+  # writecolumns = ["{}".format("file"), "TP", "FP", "FN", "TN", "Precision", "Recall", "IoU"]
+  writecolumns0 = ["file", "Precision(px)", "Recall(px)", "IoU(px)"]
+  with open(os.path.join(FLAGS.output_dir, FLAGS.output_data), 'w') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar='\\', lineterminator='\n')
+    writer.writerow(writecolumns0)
+
+  sum_TP = 0
+  sum_FP = 0
+  sum_FN = 0
+  sum_TN = 0
+
+  for inp, tar, out in zip(input_list, target_list, output_list):
+    img_input = cv2.imread(inp)
+    img_target = cv2.imread(tar)
+    img_output = cv2.imread(out)
+
+    input_base = os.path.splitext(os.path.basename(inp))[0]
+
+    print(inp, tar, out)
+
+    img_overlay = img_target.copy()
+    img_overlay2 = img_input.copy()
+    img_contour = img_input.copy()
+
+    cond_target_f = (img_target[:, :, 0] == 255) & \
+                        (img_target[:, :, 1] == 255) & \
+                        (img_target[:, :, 2] == 255)
+    cond_target_p = np.logical_not(cond_target_f)
+
+    cond_output_f = (img_output[:, :, 0] == 255) & \
+                        (img_output[:, :, 1] == 255) & \
+                        (img_output[:, :, 2] == 255)
+    cond_output_p = np.logical_not(cond_output_f)
+
+    cond_overlay_TP = np.logical_and(cond_output_p, cond_target_p)
+    cond_overlay_FP = np.logical_and(cond_output_p, cond_target_f)
+    cond_overlay_FN = np.logical_and(cond_output_f, cond_target_p)
+    cond_overlay_TN = np.logical_and(cond_output_f, cond_target_f)
+
+    img_overlay[cond_overlay_TP] = [0, 0, 255]
+    img_overlay[cond_overlay_FP] = [0, 255, 0]
+    img_overlay[cond_overlay_FN] = [255, 255, 0]
+    img_overlay[cond_overlay_TN] = [255, 255, 255]
+
+    overlay_name = os.path.join(FLAGS.output_dir, input_base + "_overlay.png")
+    cv2.imwrite(overlay_name, img_overlay)
+
+
+    for color in [(0, 0, 255), (0, 255, 0), (255, 255, 0)]:
+      black = cv2.inRange(img_overlay, color, color)
+      contours, _ = cv2.findContours(black, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+      img_contour = cv2.drawContours(img_contour, contours, -1, color, 1)
+
+    img_overlay[cond_overlay_TN] = [0, 0, 0]
+    img_overlay2 = cv2.addWeighted(img_contour, 1, img_overlay, 0.2, 0)
+    overlay2_name = os.path.join(FLAGS.output_dir, input_base + "_overlay2.png")
+    cv2.imwrite(overlay2_name, img_overlay2)
+
+    px_TP = np.count_nonzero(cond_overlay_TP)
+    px_FP = np.count_nonzero(cond_overlay_FP)
+    px_FN = np.count_nonzero(cond_overlay_FN)
+    px_TN = np.count_nonzero(cond_overlay_TN)
+
+    precision_px = px_TP / (px_TP + px_FP)
+    recall_px = px_TP / (px_TP + px_FN)
+    iou_px = px_TP / (px_TP + px_FP + px_FN)
+
+    # print(px_TP, px_FP, px_FN, px_TN, px_all, px, precision, recall, iou)
+    writecolumns = [os.path.basename(overlay_name), "{:.4f}".format(precision_px), "{:.4f}".format(recall_px), "{:.4f}".format(iou_px)]
+    with open(os.path.join(FLAGS.output_dir, FLAGS.output_data), 'a', encoding='cp932') as f:
+      writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar='\\', lineterminator='\n')
+      writer.writerow(writecolumns)
+
+    sum_TP += px_TP
+    sum_FP += px_FP
+    sum_FN += px_FN
+    sum_TN += px_TN
+
+  precision_all = sum_TP / (sum_TP + sum_FP)
+  recall_all = sum_TP / (sum_TP + sum_FN)
+  iou_all = sum_TP / (sum_TP + sum_FP + sum_FN)
+
+  writecolumns_all = ["sum",  "{:.4f}".format(precision_all), "{:.4f}".format(recall_all), "{:.4f}".format(iou_all)]
+  with open(os.path.join(FLAGS.output_dir, FLAGS.output_data), 'a',encoding='cp932') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar='\\', lineterminator='\n')
+    writer.writerow(writecolumns_all)
+
+
+def main():
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--test_dir', type=str, default='results/CS_pix2pix/test_latest/images_tf')
+
+  parser.add_argument('--input_dir', type=str, default='org_image/input')
+
+  parser.add_argument('--target_dir', type=str, default='org_image/target')
+
+  parser.add_argument('--output_dir', type=str, default='output')
+
+  parser.add_argument('--output_data', type=str, default='out.csv')
+
+  FLAGS = parser.parse_args()
+  # print(FLAGS)
+
+  merge(FLAGS)
+  analyze(FLAGS)
+
+
+if __name__ == '__main__':
+
+  main()
---
+++ preprocess.py
@@ -0,0 +1,195 @@
+import argparse
+import glob
+import os
+
+import cv2
+import numpy as np
+
+
+def preparation(src_dir, subset_name):
+    if not os.path.isdir(src_dir):
+        raise FileNotFoundError(f"Could not find {src_dir}.")
+
+    dst_dir = os.path.join(src_dir + "_" + subset_name)
+    if not os.path.isdir(dst_dir):
+        os.makedirs(dst_dir)
+
+    image_list = sorted(glob.glob(os.path.join(src_dir, "*.png")))
+
+    return image_list, dst_dir
+
+
+def crop(length, l_split, overlap):
+
+    if length <= l_split:
+        position = [(0, length)]
+        n_crop = 1
+    elif overlap != 0:
+        px_overlap = int(l_split * overlap)
+        start_next = 0
+        n_crop = int((length - px_overlap) / (l_split - px_overlap))
+        position = list(range(n_crop))
+        for i_crop in range(n_crop):
+            start = start_next
+            end = start + l_split
+            start_next = end - px_overlap
+            position[i_crop] = (start, end)
+    else:
+        n_crop, rem = divmod(length, l_split)
+        if rem == 0:
+            px_overlap = 0
+            err = 0
+        else:
+            px_overlap = (l_split - rem) // n_crop
+            sum_crop = (l_split - px_overlap) * n_crop + l_split
+            err = sum_crop - length
+            n_crop += 1
+        position = list(range(n_crop))
+        start_next = 0
+        for i_crop in range(n_crop):
+            start = start_next
+            end = start + l_split
+            start_next = end - px_overlap
+            if i_crop < err:
+                start_next -= 1
+        # print(i_h, left, right, left_next)
+            position[i_crop] = (start, end)
+      # print(pos_h)
+    return position
+
+
+def resize(input_size, input_res, split_size, split_tile):
+    target_size = split_tile / split_size
+    factor = input_res / target_size
+    resize_size = round(input_size * factor)
+    return resize_size
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_dir', type=str, default='input')
+
+    parser.add_argument('--target_dir', type=str, default='target')
+
+    parser.add_argument('--split_h', type=int, default=256)
+
+    parser.add_argument('--split_w', type=int, default=256)
+
+    parser.add_argument('--split_name', type=str, default='split')
+
+    parser.add_argument('--resize', action='store_true')
+
+    parser.add_argument('--resize_name', type=str, default='resize')
+
+    parser.add_argument('--input_res_h', type=float, default=1.0)
+
+    parser.add_argument('--input_res_w', type=float, default=1.0)
+
+    parser.add_argument('--split_tile_h', type=float, default=1000)
+
+    parser.add_argument('--split_tile_w', type=float, default=1000)
+
+    parser.add_argument('--exclude', action='store_true')
+
+    parser.add_argument('--exclude_th', type=float, default=0)
+
+    parser.add_argument('--overlap', type=float, default=0)
+
+    parser.add_argument('--num_start', type=int, default=0, choices=[0, 1])
+
+    FLAGS = parser.parse_args()
+
+    h_split = FLAGS.split_h
+    w_split = FLAGS.split_w
+
+    input_list, input_split_dir = preparation(FLAGS.input_dir, FLAGS.split_name)
+    target_list, target_split_dir = preparation(FLAGS.target_dir, FLAGS.split_name)
+    if FLAGS.resize:
+        _, input_resize_dir = preparation(FLAGS.input_dir, FLAGS.resize_name)
+        _, target_resize_dir = preparation(FLAGS.target_dir, FLAGS.resize_name)
+
+    for input_file, target_file in zip(input_list, target_list):
+        input_name = os.path.splitext(os.path.basename(input_file))[0]
+        target_name = os.path.splitext(os.path.basename(target_file))[0]
+        # print(input_name, target_name)
+
+        img1 = cv2.imread(input_file)
+        img2 = cv2.imread(target_file)
+
+        if img1.shape != img2.shape:
+            raise ValueError("The size of input {} does not match with that of target {}.".format(input_name, target_name))
+
+        if FLAGS.resize:
+            dsize_h = resize(img1.shape[0], FLAGS.input_res_h, h_split, FLAGS.split_tile_h)
+            dsize_w = resize(img1.shape[1], FLAGS.input_res_w, w_split, FLAGS.split_tile_w)
+            # print(img1.shape)
+            # print(dsize_h, dsize_w)
+
+            img1_r = cv2.resize(img1, (dsize_w, dsize_h))
+            img2_r = cv2.resize(img2, (dsize_w, dsize_h))
+
+            # if FLAGS.overlap == 0:
+            #     img1_s = img1_r
+            #     img2_s = img2_r
+            # else:
+            #     n_h_o, rem_h_o = divmod(dsize_w, w_split)
+            #     margin_L = -(- rem_h_o // 2)
+            #     margin_R = dsize_w - (rem_h_o - margin_L)
+
+            #     n_v_o, rem_v_o = divmod(dsize_h, h_split)
+            #     margin_T = -(- rem_v_o // 2)
+            #     margin_B = dsize_h - (rem_v_o - margin_T)
+                
+            #     img1_s = img1_r[margin_T:margin_B, margin_L:margin_R, :]
+            #     img2_s = img2_r[margin_T:margin_B, margin_L:margin_R, :]
+
+            img1_s = img1_r
+            img2_s = img2_r
+
+            input_resize = os.path.join(input_resize_dir, os.path.basename(input_file))
+            target_resize = os.path.join(target_resize_dir, os.path.basename(target_file))
+            cv2.imwrite(input_resize, img1_s)
+            cv2.imwrite(target_resize, img2_s)
+            # print(img1_s.shape, img2_s.shape)
+        else:
+            img1_s = img1
+            img2_s = img2
+
+        # sys.exit()
+
+        # h1, w1 = img1.shape[:2]
+        # h2, w2 = img2.shape[:2]
+        height, width = img1_s.shape[:2]
+        # px_overlap_h = h_split * FLAGS.overlap
+
+        pos_h = crop(width, w_split, FLAGS.overlap)
+        pos_v = crop(height, h_split, FLAGS.overlap)
+        # print(width, height, n_h, n_v, rem_h, rem_v)
+        # print(overlap_h, overlap_v, sum_h, sum_v, err_h, err_v)
+
+        for i_v in range(len(pos_v)):
+            for i_h in range(len(pos_h)):
+                # print(i_h, i_v, pos_h[i_h], pos_v[i_v], pos_h[i_h][0], pos_h[i_h][1], pos_v[i_v][0], pos_v[i_v][1])
+                img1_split = img1_s[pos_v[i_v][0]:pos_v[i_v][1], pos_h[i_h][0]:pos_h[i_h][1], :]
+                img2_split = img2_s[pos_v[i_v][0]:pos_v[i_v][1], pos_h[i_h][0]:pos_h[i_h][1], :]
+
+                if FLAGS.exclude:
+                    if np.all(img2_split == 255):
+                        continue
+
+                    if FLAGS.exclude_th != 0:
+                        if (np.count_nonzero(np.all(img2_split != [255,255,255], axis=2)) < FLAGS.exclude_th * h_split * w_split):
+                            continue
+                
+                i_v_n = i_v + FLAGS.num_start
+                i_h_n = i_h + FLAGS.num_start
+                split_name = "{}_{:02d}_{:02d}.png".format(input_name, i_v_n, i_h_n)
+                input_split = os.path.join(input_split_dir, split_name)
+                cv2.imwrite(input_split, img1_split)
+                target_split = os.path.join(target_split_dir, split_name)
+                cv2.imwrite(target_split, img2_split)
+
+
+if __name__ == "__main__":
+    main()
